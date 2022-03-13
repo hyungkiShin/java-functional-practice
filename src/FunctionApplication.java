5fd157a4747905dbc87525273e844b431a7c481f
hyungkiShin <tkaqkeldk@nate.com>
2022-03-13 17:39:47 +0900

    [Lambada] Class 없이 lambda 구현

--- src/FunctionApplication.java
+++ src/FunctionApplication.java
@@ -4,7 +4,8 @@ import java.util.function.Function;
 
 public class FunctionApplication {
     public static void main(String[] args) {
-        Function<Integer, Integer> myAdder = new Adder();
+//        Function<Integer, Integer> myAdder = new Adder();
+        Function<Integer, Integer> myAdder = x -> x + 10;;
         int result = myAdder.apply(1);
         System.out.println("myAdder.apply(1) = " + result);
     }
