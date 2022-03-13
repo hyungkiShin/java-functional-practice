b0630043f6be209dac90dffb10f35d99924de006
hyungkiShin <tkaqkeldk@nate.com>
2022-03-13 18:15:22 +0900

    [Custom Function Interface] add

---
+++ src/FunctionApplication3.java
@@ -0,0 +1,10 @@
+import util.TriFunction;
+
+public class FunctionApplication3 {
+    public static void main(String[] args) {
+        TriFunction<Integer, Integer, Integer, Integer> addThreeNumbers =
+                (x,y,z) -> x+y+z;
+        int result = addThreeNumbers.apply(1,2,3);
+        System.out.println("result = " + result);
+    }
+}
